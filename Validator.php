df582142ac99a94588be399ea18c987a5a4960d2
Anthony Gonzalez <darkdevilishag@gmail.com>
2015-10-02 18:24:50 -0400

    Adds validate_presences()

--- Validator.php
+++ Validator.php
@@ -2,8 +2,29 @@
 
 abstract class Validator {
 
+	private static $errors = array();
+
+	static function errors() {
+		return self::$errors;
+	}
+
 	static function has_presence($value) {
 		return isset($value) && $value !== "";
+	}
+
+	static function validate_presences($required_fields) {
+		foreach($required_fields as $field=>$value) {
+    	$value = trim($value);
+  		if (!self::has_presence($value)) {
+  			self::$errors[$field] = self::fieldname_as_text($field) . " can't be blank";
+  		}
+  	}
+	}
+
+	private static function fieldname_as_text($fieldname) {
+		$fieldname = str_replace("-", " ", $fieldname);
+  	$fieldname = ucfirst($fieldname);
+  	return $fieldname;
 	}
 
 }
--- tests/unit/ValidatorTest.php
+++ tests/unit/ValidatorTest.php
@@ -11,8 +11,20 @@ class ValidatorTest extends \Codeception\TestCase\Test {
     $this->assertEquals(true, $value);
   }
 
-  function test_validates_presences() {
-    //$values = array('name' => 'John', 'username' => 'chacala');
+  function test_validate_presences() {
+    $values = array('name' => 'John', 'username' => 'chacala');
+    $empty_values = array('name' => '     ', 'username' => '    ');
+    $empty_value = array('last-name' => '', 'username' => 'whatever');
+    Validator::validate_presences($values);
 
+    $this->assertEmpty( Validator::errors() );
+    
+    Validator::validate_presences($empty_values);
+
+    $this->assertNotEmpty( Validator::errors() );
+
+    Validator::validate_presences($empty_value);
+
+    $this->assertEquals( "Last name can't be blank", Validator::errors()["last-name"] );
   }
 }
